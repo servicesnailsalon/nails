7a99172558b7ab3372c9a01c4955b7daa3ce81fb
All Perfect Nails <tichphiduong@gmail.com>
2026-04-16 14:24:35 -0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig({
+  base: '/nails/', 
+});
+const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
     define: {
